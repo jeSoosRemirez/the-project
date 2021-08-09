d9982d374dae75bf94dbc37ede64deeddd88c8be
jeSoosRemirez <dkhomych0091@gmail.com>
2021-08-09 11:08:36 +0300

    Added temporal students data

--- students/views.py
+++ students/views.py
@@ -5,7 +5,30 @@ from django.http import HttpResponse
 # Views for Students
 
 def students_list(request):
-    return render(request, "students/students_list.html", {})
+    students = (
+        {
+            'id': 1,
+            'first_name': 'Viktor',
+            'last_name': 'Kapusta',
+            'ticket': 742,
+            'image': 'img/kapusta.png'
+        },
+        {
+            'id': 2,
+            'first_name': 'Vitaliy',
+            'last_name': 'Tsal',
+            'ticket': 617,
+            'image': 'img/papich.png'
+        },
+        {
+            'id': 3,
+            'first_name': 'Oleg',
+            'last_name': 'Negdanchik',
+            'ticket': 27,
+            'image': 'img/bruh.png'
+        }
+    )
+    return render(request, "students/students_list.html", {'students': students})
 
 
 def students_add(request):
