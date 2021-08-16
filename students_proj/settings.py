f8498e9783c92983f08feb806a25c2c9cb8d6aeb
jeSoosRemirez <dkhomych0091@gmail.com>
2021-08-16 12:14:08 +0300

    Added context proc in templates and created PORTAL_URL

--- students_proj/settings.py
+++ students_proj/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
+PORTAL_URL = 'http://localhost:8000'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -63,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'students_proj.context_processors.students_proc',
             ],
         },
     },
