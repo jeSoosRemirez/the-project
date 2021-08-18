6bb3e6e1f4d9c87acf7e6fea7626da4bae1139e5
jeSoosRemirez <dkhomych0091@gmail.com>
2021-08-18 11:46:01 +0300

    Imported views pack and renamed few things

--- students_proj/urls.py
+++ students_proj/urls.py
@@ -16,21 +16,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from students import views as stud_views
+from students.templates.views import students_views, groups_views
 
 
 urlpatterns = [
     # Students urls
-    path('', stud_views.students_list, name='home'),
-    path('students/add/', stud_views.students_add, name='students_add'),
-    path('students/<int:sid>/edit/', stud_views.students_edit, name='students_edit'),
-    path('students/<int:sid>/delete/', stud_views.students_delete, name='students_delete'),
+    path('', students_views.students_list, name='home'),
+    path('students/add/', students_views.students_add, name='students_add'),
+    path('students/<int:sid>/edit/', students_views.students_edit, name='students_edit'),
+    path('students/<int:sid>/delete/', students_views.students_delete, name='students_delete'),
 
     # Groups urls
-    path('groups/', stud_views.groups_list, name='groups'),
-    path('groups/add/', stud_views.groups_add, name='groups_add'),
-    path('groups/<int:gid>/edit/', stud_views.groups_edit, name='groups_edit'),
-    path('groups/<int:gid>/delete/', stud_views.groups_delete, name='groups_delete'),
+    path('groups/', groups_views.groups_list, name='groups'),
+    path('groups/add/', groups_views.groups_add, name='groups_add'),
+    path('groups/<int:gid>/edit/', groups_views.groups_edit, name='groups_edit'),
+    path('groups/<int:gid>/delete/', groups_views.groups_delete, name='groups_delete'),
 
     # Admin
     path('admin/', admin.site.urls),
