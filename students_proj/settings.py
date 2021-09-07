95a99b71f34d7546182b463aa44d99b6c643e641
jeSoosRemirez <dkhomych0091@gmail.com>
2021-09-07 16:59:28 +0300

    Moved DATABASES into db.py

--- students_proj/settings.py
+++ students_proj/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import os
-
+from . import db
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -65,12 +65,7 @@ WSGI_APPLICATION = 'students_proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
-    }
-}
+DATABASES = db.DATABASES
 
 
 # Password validation
