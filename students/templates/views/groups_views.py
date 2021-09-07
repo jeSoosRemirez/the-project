704e7408e8b90a70aa6a3a6cd42d225ad326ec69
jeSoosRemirez <dkhomych0091@gmail.com>
2021-09-07 09:47:36 +0300

    Added groups views with pagination and sorting

--- students/templates/views/groups_views.py
+++ students/templates/views/groups_views.py
@@ -1,25 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from students.models import Group
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 def groups_list(request):
-    groups = (
-        {
-            'id': 1,
-            'naming': 'A',
-            'head': 'Kapusta',
-        },
-        {
-            'id': 2,
-            'naming': 'B',
-            'head': 'Tsal',
-        },
-        {
-            'id': 3,
-            'naming': 'C',
-            'head': 'Bruh',
-        }
-    )
+    groups = Group.objects.all()
+
+    # order groups
+    order_by = request.GET.get('order_by', '')
+    if order_by in ('title', 'leader'):
+        groups = groups.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            groups = groups.reverse()
+
+    # paginate groups
+    paginator = Paginator(groups, 3)
+    page = request.GET.get('page')
+    try:
+        groups = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer then deliver first page
+        groups = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range (9999) then deliver last page of res
+        groups = paginator.page(paginator.num_pages)
     return render(request, 'students/groups.html', {'groups': groups})
 
 
